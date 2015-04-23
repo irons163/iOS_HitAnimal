0874c8d20345cdb4f9c0f7bc285e5d9ef225304e
irons163 <irons@ironsdeMacBook-Air.local>
2015-04-23 10:00:06 +0800

    Initial Commit

---
+++ Try_HitAnimalTests/Try_HitAnimalTests.m
@@ -0,0 +1,34 @@
+//
+//  Try_HitAnimalTests.m
+//  Try_HitAnimalTests
+//
+//  Created by irons on 2015/4/23.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Try_HitAnimalTests : XCTestCase
+
+@end
+
+@implementation Try_HitAnimalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
